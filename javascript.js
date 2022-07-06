8891f7001e291c63492fb34444fbe05e736411c0
Lawhornmatt <66797498+Lawhornmatt@users.noreply.github.com>
2022-07-06 16:09:43 -0500

    Minimal functionality acheived

--- javascript.js
+++ javascript.js
@@ -1,1 +1,37 @@
-//Main file where I'll add javastuff
+// Assignment code here
+
+var characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+
+var newLetter = "";
+var emptyPassword = "";
+
+function resetPassword() {
+  emptyPassword = "";
+}
+
+function generatePassword() {
+    for (let i = 0; i < 5; i++) {
+      newLetter = characters[Math.floor(Math.random() * characters.length)];
+      // console.log(newLetter); DEBUG
+      emptyPassword += newLetter;
+    }
+    // console.log(emptyPassword); DEBUG
+    return emptyPassword;
+}
+
+
+// Get references to the #generate & #password elements
+var generateBtn = document.getElementById("generate");
+var passwordText = document.getElementById("password");
+
+// Write password to the #password input
+function writePassword() {
+  // console.log("Button was pressed"); DEBUG
+  var password = generatePassword();
+  // console.log(password); DEBUG
+  passwordText.value = password;
+  resetPassword();
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
